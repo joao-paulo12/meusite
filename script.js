4e90e4c82e17fbb6b36e2843aac4648cffada95a
João Paulo <joaoandrade1603@gmail.com>
2022-02-22 10:29:34 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", function(){
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+
+document.querySelector('#quantidade').addEventListener("change", atualizarPreco)
+document.querySelector('#js').addEventListener("change", atualizarPreco)
+document.querySelector('#layout-sim').addEventListener("change", atualizarPreco)
+document.querySelector('#layout-nao').addEventListener("change", atualizarPreco)
+document.querySelector('#prazo').addEventListener("change", function (){
+    const prazo = document.querySelector('#prazo').value
+    document.querySelector('label[for=prazo]').innerHTML = `${prazo} semana(s)`
+    atualizarPreco()
+})
+
+function atualizarPreco(){
+    const quantidade = document.querySelector('#quantidade').value
+    const temJS = document.querySelector('#js').checked
+    const incluiLayout = document.querySelector('#layout-sim').checked
+    const prazo = document.querySelector('#prazo').value
+
+    let preco = quantidade * 100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 200
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+    document.querySelector('#preco').innerHTML = `R$ ${preco.toFixed(2)}`
+}
